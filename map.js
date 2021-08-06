6ea926b69a4522a9fda62393d3bf057bf7542d9a
Sandeep Kumar <Sandeepkumar50220@gmail.com>
2021-08-06 13:16:20 +0530

    map&coustummap

---
+++ map.js
@@ -0,0 +1,25 @@
+// Map is a method which work on Array 
+// it take function as argumet and call that function on each value of Array 
+
+let arr=[1,2,3,4,5];
+function double(x){
+return 2*x;
+}
+
+
+let ans=arr.map(double);
+ console.log("original array :" + arr);
+ console.log("Map answer :"+ans);
+
+function coustumMapFunction(arr,fun){
+let coustum=[];
+for(let i=0;i<arr.length;i++){
+
+    coustum.push(fun(arr[i]));
+}
+
+return ans;
+
+}
+
+console.log("coustum wala map :"+ coustumMapFunction(arr,double));
